1b31e0a5ee89f264951f34843d4db267819d74bb
C. Saxby Bain <csaxbybain@yahoo.com>
2024-11-18 14:36:12 -0500

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+var box1 = document.querySelector(".box-1");
+var box2 = document.querySelector(".box-2");
+var box3 = document.querySelector(".box-3");
+
+document.addEventListener("keydown", function (e) {
+    var key = e.key;
+    if (key === "1") {
+        box1.innerText = "🤗";
+    } else if (key === "2") {
+        box2.innerText ="🤠";
+    } else if (key === "3") {
+        box3.innerText = "👾";
+    }
+
+});
